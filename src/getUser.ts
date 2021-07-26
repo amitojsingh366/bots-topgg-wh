4412d518417afc75870351d21410705043978d09
Amitoj Singh <amitojsingh366@gmail.com>
2021-07-26 05:44:21 +0300

    fix bug

--- src/getUser.ts
+++ src/getUser.ts
@@ -2,15 +2,15 @@ import { User } from "./types/user";
 import axios from 'axios';
 
 export async function getUser(id: string): Promise<User> {
-    let res = await axios.get(`https://discord.com/api/v8/users/201345371513946112`, {
+    let res = await axios.get(`https://discord.com/api/v8/users/${id} `, {
         headers: {
-            'authorization': `Bot ${process.env.BOT_TOKEN}`
+            'authorization': `Bot ${process.env.BOT_TOKEN} `
         }
     })
 
     return {
-        id: res.data.data.id,
-        tag: `${res.data.username}#${res.data.discriminator}`,
+        id: res.data.id,
+        tag: `${res.data.username}#${res.data.discriminator} `,
         username: res.data.username,
         avatar: `https://cdn.discordapp.com/avatars/${res.data.id}/${res.data.avatar}.webp`,
         discriminator: res.data.discriminator,
