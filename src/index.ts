e0a42d19a70d8e4a81ec5a1694ce484a1c595fdf
Amitoj Singh <amitojsingh366@gmail.com>
2021-07-26 04:59:53 +0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import topgg from "@top-gg/sdk";
 import express from "express";
 import { config } from "dotenv";
 import { getUser } from "./getUser";
 import { sendMessage } from "./sendMessage";
 
 config();
+
+const Topgg = require("@top-gg/sdk");
 
 const WH_SECRET = process.env.WH_SECRET;
 const PORT = process.env.PORT;
@@ -13,7 +14,7 @@ const DISC_WH = process.env.DISC_WH;
 if (!WH_SECRET || !PORT || !DISC_WH) process.exit();
 
 const app = express()
-const webhook = new topgg.Webhook(WH_SECRET)
+const webhook = new Topgg.Webhook(WH_SECRET)
 
 app.post("/dblwebhook", webhook.listener(async (vote) => {
     const bot = await getUser(vote.bot || "");
@@ -22,7 +23,7 @@ app.post("/dblwebhook", webhook.listener(async (vote) => {
     await sendMessage(bot, user, vote);
 }))
 
-app.listen(PORT)
+app.listen(PORT, () => console.log(`wh running on: http://localhost:${PORT}/dblwebhook`))
 
 
 
